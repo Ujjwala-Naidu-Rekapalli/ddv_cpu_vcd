4fbd178af302757ece71478d9870b46f7c291f22
Ujjwala Naidu Rekapalli <urekapal@asu.edu>
2025-09-07 17:07:34 -0700

    Flatten repo layout; keep one small VCD demo

--- ddv_cpu_vcd/ddv_cpu_sources/tb/cpu_mon_if.sv
+++
@@ -1,9 +0,0 @@
-// tb/cpu_mon_if.sv
-interface cpu_mon_if(input logic clk);
-  logic [31:0] pc;
-  logic        branch_taken;
-  logic        stall;
-  logic [1:0]  forwardA;
-  logic [1:0]  forwardB;
-endinterface
-
